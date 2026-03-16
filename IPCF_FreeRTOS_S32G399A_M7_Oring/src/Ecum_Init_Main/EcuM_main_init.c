c0d48257dbd8397add660b43515785465f859914
weizhichun <361873098@qq.com>
2026-03-16 15:37:01 +0800

    p:sdv-5083 tja1145 optimus
    delet no use function

--- IPCF_FreeRTOS_S32G399A_M7_Oring/src/Ecum_Init_Main/EcuM_main_init.c
+++ IPCF_FreeRTOS_S32G399A_M7_Oring/src/Ecum_Init_Main/EcuM_main_init.c
@@ -92,7 +92,7 @@ int main(void) {
   /* Initialize pins */
   Port_Init(NULL_PTR);
 
-  Siul2_Port_Ip_Init(NUM_OF_CONFIGURED_PINS, g_pin_mux_InitConfigArr);
+  //Siul2_Port_Ip_Init(NUM_OF_CONFIGURED_PINS, g_pin_mux_InitConfigArr);
 
   /* Initialize I2c driver */
   I2c_Init(NULL_PTR);
